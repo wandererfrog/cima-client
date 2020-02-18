e30329a314779dc4a00b2e6e86c53ef096d19dad
tobirama <tobirama@tobiramas-iMac.local>
2020-02-18 19:26:02 +0100

    updates config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  apiUrl: "http://localhost:8080/api/v1",
+  apiUrl: "http://api-cima.herokuapp.com/api/v1",
   CMS_API: "https://api-euwest.graphcms.com/v1/ck68b6a864txh01936pghdli0/master"
 };
