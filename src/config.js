c5191263fcfc0bab48005a978e8bfcac74f84955
tobirama <tobirama@tobiramas-iMac.local>
2020-02-18 19:37:53 +0100

    https...

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  apiUrl: "http://api-cima.herokuapp.com/api/v1",
+  apiUrl: "https://api-cima.herokuapp.com/api/v1",
   CMS_API: "https://api-euwest.graphcms.com/v1/ck68b6a864txh01936pghdli0/master"
 };
